e557cd107ff7ca7ea7a63e0128fd7880af96c53b
OndraKnow <ondra.polanecky@seznam.cz>
2020-01-27 09:07:08 +0100

    ahoj

--- hra.cpp
+++ hra.cpp
@@ -1,3 +1,5 @@
+
+#include <ncurses.h>
 struct hra
 {
     virtual bool checkWin() = 0;
@@ -16,9 +18,51 @@ struct hra
 };
 
 
+struct mojeHra : hra
+{
+    char p1;
+    char p2;
+    
+    int turnCount;
+    mojeHra(){
+        turnCount = 0;
+    }
+    virtual bool checkWin(){
+        if(turnCount < 2){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    virtual void makeMove(){
+        if(turn){
+            p1 = getch();
+        }
+        else{
+            p2 = getch();
+        }
+        turnCount++;
+    }
+
+    virtual void printResult(){
+        if (p1 + 10 <= p2 and p1 - 10 >= p2 ){
+            printf("vyhral hrac 1 s pismenem %c",p1);
+        }
+        else
+        {
+            printf("vyhral hrac 2 s pismenem %c",p2);
+        }
+        
+    }
+};
+
+
 
 
 
 int main(int argc, char *argv[]){
-
+    initscr();
+    mojeHra mojehra;
+    mojehra.startGame();
 }
