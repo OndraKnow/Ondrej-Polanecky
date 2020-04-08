dfc23910c9bfee056ba584058c5bf9ec5c4b7b0c
Jan Turon <janturon@email.cz>
2020-04-08 09:09:37 +0200

    hra.cpp

--- hra.cpp
+++ hra.cpp
@@ -1,3 +1,6 @@
+#include <conio.h>
+#include <cstdio>
+
 struct hra
 {
     virtual bool checkWin() = 0;
@@ -15,10 +18,37 @@ struct hra
     }
 };
 
+struct mojehra : hra {
+    int moves;
+    char p1guess, p2guess;
 
+    mojehra() {
+      moves = 0;
+    }
+
+    virtual bool checkWin() {
+      if(moves<2) return false;
+      else return true;
+    }
+
+    virtual void makeMove() {
+      if(turn) p2guess = getch();
+      else p1guess = getch();
+      moves++;
+    }
+
+    virtual void printResult() {
+      printf("P1 hadal %c, P2 hadal %c.\n", p1guess, p2guess);
+      if(p1guess>p2guess) puts("Vyhral P1");
+      else if(p1guess<p2guess) puts("Vyhral P2");
+      else puts("Remiza");
+    }
+
+};
 
 
 
 int main(int argc, char *argv[]){
-
+  mojehra stupidGame;
+  stupidGame.startGame();
 }
